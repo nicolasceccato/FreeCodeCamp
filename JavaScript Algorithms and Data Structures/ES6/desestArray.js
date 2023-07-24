99481907f424659d7d15f02c510449315f266428
nicolasceccato <nicolasceccato@hotmail.com>
2023-07-24 20:42:18 -0300

    desestrutrando arrays

---
+++ JavaScript Algorithms and Data Structures/ES6/desestArray.js
@@ -0,0 +1,6 @@
+/*Use atribuição de desestruturação para trocar os valores de a e b para que a receba os valores armazenados em b e b recebe os valores armazenados em a.
+*/
+
+let a = 8, b = 6;
+// Altere apenas o código abaixo desta linha
+[a, b] = [b, a];
