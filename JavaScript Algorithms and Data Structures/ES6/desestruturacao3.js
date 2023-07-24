842c39a3e627f251cfa94c733f0e5eda310dcb00
nicolasceccato <nicolasceccato@hotmail.com>
2023-07-24 20:33:26 -0300

    desestruturacao3

---
+++ JavaScript Algorithms and Data Structures/ES6/desestruturacao3.js
@@ -0,0 +1,11 @@
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// Altere apenas o código abaixo desta linha
+
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+
+  // Altere apenas o código acima desta linha
