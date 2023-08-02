a09aa591259df72b7bee4dfabcd5fc7725da3ae1
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-02 09:10:15 -0300

    usando console dot log

---
+++ JavaScript Algorithms and Data Structures/Debugging/consoleLog.js
@@ -0,0 +1,18 @@
+/*O Chrome e o Firefox possui excelentes consoles JavaScript, também conhecidos como DevTools, para depurar seu JavaScript.
+
+Você pode encontrar as Developer tools no menu do seu Chrome ou Web Console no menu do Firefox. Se você estiver usando um navegador diferente, ou um telefone móvel, recomendamos fortemente mudar para o Firefox ou Chrome Desktop.
+
+O método console.log(), o qual "imprime" a saída do que está nos seus parênteses no console, provavelmente será a ferramenta de debug mais útil. Colocá-lo em pontos estratégicos no seu código pode mostrar a você os valores intermediários de variáveis. É uma boa prática ter uma ideia do que deveria ser a saída antes de olhar o que é. Ter pontos de verificação para ver o status de seus cálculos ao longo do seu código ajudará a encontrar onde o problema está.
+
+Aqui está um exemplo para imprimir a string Hello world! no console:
+
+console.log('Hello world!');
+Use o método console.log() para imprimir o valor da variável a aonde anotou no código.*/
+let a = 5;
+let b = 1;
+a++;
+// Altere apenas o código abaixo desta linha
+
+console.log(a)
+let sumAB = a + b;
+console.log(sumAB);
