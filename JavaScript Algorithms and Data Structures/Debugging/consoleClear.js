9f4a2bfd939ca067f5c8e81474a18bb471ecaac7
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-02 09:16:28 -0300

    usando clear no console

---
+++ JavaScript Algorithms and Data Structures/Debugging/consoleClear.js
@@ -0,0 +1,11 @@
+/*Você pode ter percebido que alguns dos desafios do freeCodeCamp incluem seu próprio console. Este console se comporta um pouco diferente do console do navegador.
+
+Existem muitos métodos para usar no console para exibir mensagens. log, warn e clear são alguns deles. O console do freeCodeCamp só produzirá mensagens de log. Já o console do navegador mostrará todas as mensagens. Quando você fizer alterações em seu código, ele será executado e mostrará automaticamente os logs. O console do freeCodeCamp é limpo cada vez que o código é executado.
+
+Primeiro, abra o console do navegador para poder ver os registros (logs). Para fazer isso, clique com o botão direito na barra de navegação do freeCodeCamp, na parte superior, e clique em inspect na maioria dos navegadores. Em seguida, encontre a aba console na janela que é aberta.
+
+Depois disso, use console.log para registrar a variável output. Veja os dois consoles para ver o log. Por fim, use console.clear depois do registro para limpar o console do navegador. Veja a diferença entre os dois consoles.*/
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear()
